ba4c85cdef6282b550c011d527a516711b9e899b
Walghamdi97 <wejgh1997@gmail.com>
2025-01-19 10:26:00 +0300

    Initial Commit

---
+++ Board_Rooms/Board_RoomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Board_RoomsApp.swift
+//  Board_Rooms
+//
+//  Created by Wejdan Alghamdi on 19/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Board_RoomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
